e361a57a07c47a2a5c1af318d442bffddd72ee22
Your Name <you@example.com>
2024-11-03 23:41:19 +0100

    Add support for parsing weapon data and introduce weapon type and mask constants

--- main.js
+++ main.js
@@ -1235,11 +1235,40 @@ class DatParser {
                 return this.loadCharacterData(stream, version, objVersion);
             case 'scroll':
                 return this.loadScrollData(stream, version, objVersion);
+            case 'weapon':
+                return this.loadWeaponData(stream, version, objVersion);
             default:
                 console.warn(`Unknown object class: ${objClassName}`);
                 debugger;
                 return {};
         }
+    }
+
+    static loadWeaponData(stream, version, objVersion) {
+        // Load base object data first
+        const baseData = this.loadBaseObjectData(stream, version, objVersion);
+    
+        // Read poison value
+        const poison = stream.readInt32();
+    
+        return {
+            ...baseData,
+            className: 'weapon',
+            poison,
+    
+            // Add helper methods and getters for stats
+            getPoison: () => poison,
+            getType: () => baseData.stats?.find(s => s.name === "Type")?.value ?? 0,
+            getDamage: () => baseData.stats?.find(s => s.name === "Damage")?.value ?? 0,
+            getEqSlot: () => baseData.stats?.find(s => s.name === "EqSlot")?.value ?? 0,
+            getCombining: () => baseData.stats?.find(s => s.name === "Combining")?.value ?? 0,
+            getValue: () => baseData.stats?.find(s => s.name === "Value")?.value ?? 0,
+    
+            // Helper method to clear weapon (matches C++ ClearWeapon())
+            clearWeapon: function() {
+                this.poison = 0;
+            }
+        };
     }
 
     static loadScrollData(stream, version, objVersion) {
@@ -1480,7 +1509,30 @@ class ExitRef {
     }
 }
 
-const RESNAMELEN = 32;
+// Weapon type constants
+const WeaponType = {
+    WT_HAND: 0,        // Hand, claw, tail, etc.   
+    WT_KNIFE: 1,       // Daggers, knives
+    WT_SWORD: 2,       // Swords
+    WT_BLUDGEON: 3,    // Clubs, maces, hammers
+    WT_AXE: 4,         // Axes
+    WT_STAFF: 5,       // Staffs, polearms, spears, etc.
+    WT_BOW: 6,         // Bow
+    WT_CROSSBOW: 7,    // Crossbow
+    WT_LAST: 7         // Last weapon type
+};
+
+// Weapon mask constants
+const WeaponMask = {
+    WM_HAND: 0x0001,
+    WM_KNIFE: 0x0002,
+    WM_SWORD: 0x0004,
+    WM_BLUDGEON: 0x0008,
+    WM_AXE: 0x0010,
+    WM_STAFF: 0x0020,
+    WM_BOW: 0x0040,
+    WM_CROSSBOW: 0x0080
+};
 
 const ActionTypes = {
     ACTION_NONE: 0,
